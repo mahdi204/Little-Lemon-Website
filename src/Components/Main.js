2b6acb27d9a4e4b865ab7f574adbd7ee0a99ef3d
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-04-26 23:40:02 +0300

    Main created

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Main = () => {
     return (
-        <div>Main</div>
+        <main className='main'>
+            <div className='cards-container'>
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src="./greek salad.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Greek Salad</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="card-btn">Go somewhere</a>
+                    </div>
+                </div>
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src="./desert.jpg" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Desert</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="card-btn">Go somewhere</a>
+                    </div>
+                </div>
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src="./food1.avif" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Fish</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="card-btn">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+        </main>
     )
 }
 
