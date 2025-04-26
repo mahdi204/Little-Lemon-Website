ed4df3ed4789007762702dca4a9ed12d8349c62b
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-04-26 16:26:40 +0300

    shop cart svg

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import React from 'react';
 import Nav from './Components/Nav';
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <Nav />
+    <>
+      <Nav />
+
+    </>
   );
 }
 
